aee186bb98639631075d404426cedf661e9d2025
maknetaRo <makneta@gmail.com>
2020-10-01 19:13:18 +0200

    change App's structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import React, { Component } from 'react';
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {counter: 0};
-  }
-  increment() {
-    this.setState({
-      counter: this.state.counter + 1
-    })
-  }
-   render(){
-      return(
-         <div>
-            <h1>Witaj jeżu</h1>
-	    <button onClick={this.increment.bind(this)}>+</button>
-            <output>{this.state.counter}</output>
-            <button>-</button>
-         </div>
-      );
-   }
-}
-export default App;
+import React from "react";
+import NameForm from "./components/NameForm";
+import Chart from "./components/Chart";
 
+const App = () => (
+  <div className="App">
+    <NameForm />
+    <Chart />
+  </div>
+);
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,29 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-   entry: './main.js',
-   devtool: 'eval-source-map',	
-   output: {
-      path: path.join(__dirname, '/bundle'),
-      filename: 'index_bundle.js'
-   },
-   devServer: {
-      inline: true,
-      port: 1337
-   },
-   module: {
-      rules: [
-         {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'ts-loader',
-
-         }
-      ]
-   },
-   plugins:[
-      new HtmlWebpackPlugin({
-         template: './index.html'
-      })
-   ]
-}
-
+  entry: "./src/main.js",
+  devtool: "eval-source-map",
+  output: {
+    path: path.join(__dirname, "/bundle"),
+    filename: "index_bundle.js",
+  },
+  devServer: {
+    inline: true,
+    port: 1337,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "ts-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
