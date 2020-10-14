8cb5980e35f4bad65a8b642b7c1f77b3cf02384c
Jarosław Kotowski <jaroslaw.kotowski@live.umcs.edu.pl>
2020-10-14 12:21:21 +0200

    modified:   src/core/Memory.test.ts

--- src/core/Memory.ts
+++ src/core/Memory.ts
@@ -1,6 +1,11 @@
+
+let savedValues= new Array();
 export function saveValues(values: { date: Date, value: number }[]): void {
+  savedValues=values;
 }
 
 export function getValues(): { date: Date, value: number }[] {
-    return [];
+  
+
+    return savedValues;
 }
