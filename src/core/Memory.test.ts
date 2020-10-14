6357a86259f08acb0ac0b29ab163fb944297ab90
Jarosław Kotowski <jaroslaw.kotowski@live.umcs.edu.pl>
2020-10-15 00:24:33 +0200

    modified:   src/core/Memory.test.ts 	modified:   src/core/Memory.ts

--- src/core/Memory.test.ts
+++ src/core/Memory.test.ts
@@ -4,8 +4,6 @@ test("Test that Value Can Be Saved And Retrieved", () => {
     const value = [{date: new Date(), value: 21}, {date: new Date(), value: 48}];
     saveValues(value);
     expect(getValues()).toEqual(value);
-    
-
 })
 
 test("Test That Value Retrieved will Be Empty If not Set", () => {
--- src/core/Memory.ts
+++ src/core/Memory.ts
@@ -5,7 +5,5 @@ export function saveValues(values: { date: Date, value: number }[]): void {
 }
 
 export function getValues(): { date: Date, value: number }[] {
-  
-
   return savedValues;
 }
