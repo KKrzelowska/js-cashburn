214f4097b03f7e5e101979cde0fec150d08ea69a
Piotr Suwała <psuwala@atlassian.com>
2020-10-15 18:59:34 +0200

    issue-7: added tests for persistent MemoryService

--- src/core/Memory.test.ts
+++ src/core/Memory.test.ts
@@ -1,11 +1,29 @@
 import {getValues, saveValues} from "./Memory";
 
+beforeEach(() => {
+    localStorage.clear();
+})
+
+const testValues = [{date: new Date(), value: 21}, {date: new Date(), value: 48}];
+
 test("Test that Value Can Be Saved And Retrieved", () => {
-    const value = [{date: new Date(), value: 21}, {date: new Date(), value: 48}];
-    saveValues(value);
-    expect(getValues()).toEqual(value);
+    saveValues(testValues);
+    expect(getValues()).toEqual(testValues);
 })
 
 test("Test That Value Retrieved will Be Empty If not Set", () => {
     expect(getValues()).toEqual([]);
+})
+
+test("Test that Saving uses local storage", () => {
+    saveValues(testValues);
+
+    expect(localStorage.setItem).toHaveBeenCalled();
+})
+
+test("Test that Loading uses local storage", () => {
+    saveValues(testValues);
+    getValues();
+
+    expect(localStorage.getItem).toHaveBeenCalled();
 })
