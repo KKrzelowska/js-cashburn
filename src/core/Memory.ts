39be7d7dc66b8e9fe456ac7f12ef37ae4ab1a8fd
Jarosław Kotowski <jaroslaw.kotowski@live.umcs.edu.pl>
2020-10-30 18:39:04 +0100

    chart.js wykresy work in progress

--- src/core/Memory.ts
+++ src/core/Memory.ts
@@ -1,6 +1,7 @@
-export function saveValues(values: { date: Date, value: number }[]): void {
+let savedValues = new Array();
+export function saveValues(values: { date: Date; value: number }[]): void {
+  savedValues = values;
 }
-
-export function getValues(): { date: Date, value: number }[] {
-    return [];
+export function getValues(): { date: Date; value: number }[] {
+  return savedValues;
 }
