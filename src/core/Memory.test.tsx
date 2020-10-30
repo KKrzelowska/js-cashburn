2b6cb762601316445cb278371c2b2e5bc9c08c58
karo <kakrzelowska@wp.pl>
2020-10-30 19:48:51 +0100

    added types to Memory

---
+++ src/core/Memory.test.tsx
@@ -0,0 +1,12 @@
+import { getValues, saveValues } from "./Memory";
+
+test("Test That Value Retrieved will Be Empty If not Set", () => {
+    expect(getValues()).toEqual([]);
+})
+
+test("Test that Value Can Be Saved And Retrieved", () => {
+    const value = [{ date: new Date(), value: 21 }, { date: new Date(), value: 48 }];
+    saveValues(value);
+    expect(getValues()).toEqual(value);
+})
+
