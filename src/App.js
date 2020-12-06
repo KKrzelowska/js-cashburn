6c91d8aa5feb8613272fc091646b47db4ff9bd71
Bartlomiej Skronski <bartlomiej.skronski@neoteric.eu>
2020-12-07 00:12:24 +0100

    test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navigation from './components/pages/Navigation';
 import NameForm from './components/pages/StatedNameForm';
-import GraphForm from './components/pages/StatedGraphForm';
+import GraphForm from "./components/pages/StatedGraphForm";
 
 const App = () => (
   <div className="App">
