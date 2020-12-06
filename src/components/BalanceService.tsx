492453bd30a5c4dd00cf1b8e815a8f038aa1e95b
KKrzelowska <kakrzelowska@wp.pl>
2020-12-06 19:16:42 +0100

    iss3: wrap MemoryService to a class

--- src/components/BalanceService.tsx
+++ src/components/BalanceService.tsx
@@ -1,19 +1,19 @@
-import {getValues, saveValues, storageKey} from "../core/Memory";
+import MemoryService from "../core/Memory";
 
 class BalanceService {
     saveValues: (balances: { date: Date, value: number }[]) => void;
     balances: { date: Date, value: number }[] ;
 
     addBalance(valueFromBalanceForm: number) {
-        let valuesDatesFromMemory = getValues();
+        let valuesDatesFromMemory = MemoryService.instance.getValues();
         let valueWithDate = {date: new Date(), value: valueFromBalanceForm};
         let valueToSave = [valueWithDate, ...valuesDatesFromMemory];
         this.balances = [valueWithDate, ...valuesDatesFromMemory];
-        saveValues(valueToSave);
+        MemoryService.instance.saveValues(valueToSave);
     }
 
     constructor() {
-        this.saveValues = (balances) => saveValues(balances);
+        this.saveValues = (balances) => MemoryService.instance.saveValues(balances);
         this.balances = [];
     };
 
--- src/core/Memory.ts
+++ src/core/Memory.ts
@@ -1,16 +1,22 @@
-export const storageKey = 'values';
+class MemoryService {
+    storageKey: string = 'values';
 
-function recoverTypesFromParsedJSON (valuesFromLocalStorage: { date: string, value: number }[]): { date: Date, value: number }[] {
-    return valuesFromLocalStorage.map(dataObj => ({
-        date: new Date(dataObj.date),
-        value: dataObj.value
-    }));
+    recoverTypesFromParsedJSON(valuesFromLocalStorage: { date: string, value: number }[]): { date: Date, value: number }[] {
+        return valuesFromLocalStorage.map(dataObj => ({
+            date: new Date(dataObj.date),
+            value: dataObj.value
+        }));
+    }
+
+    saveValues(values: { date: Date, value: number }[]): void {
+        localStorage.setItem(this.storageKey, JSON.stringify(values))
+    }
+
+    getValues(): { date: Date, value: number }[] {
+        return this.recoverTypesFromParsedJSON(JSON.parse(localStorage.getItem(this.storageKey) ?? '[]'));
+    };
+
+    static instance = new MemoryService();
 }
 
-export function saveValues(values: { date: Date, value: number }[]): void {
-    localStorage.setItem(storageKey, JSON.stringify(values))
-}
-
-export function getValues(): { date: Date, value: number }[] {
-    return recoverTypesFromParsedJSON(JSON.parse(localStorage.getItem(storageKey) ?? '[]'));
-};
+export default MemoryService;
