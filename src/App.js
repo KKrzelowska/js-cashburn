25ac29b63174aaa1986f1fdb38902b0a2f0d7135
Bartlomiej Skronski <bartlomiej.skronski@neoteric.eu>
2020-12-06 22:57:17 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import Chart from './components/Chart';
-import StatedBalanceForm from './components/StatedBalanceForm';
-import { getValues, saveValues } from './core/Memory';
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Navigation from './components/pages/Navigation';
 import NameForm from './components/pages/StatedNameForm';
 import GraphForm from './components/pages/StatedGraphForm';
