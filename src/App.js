48f3b291261d9ce82a29da12d604c022ff6e79c0
Bartlomiej Skronski <bartlomiej.skronski@neoteric.eu>
2020-12-06 22:01:08 +0100

    test

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => (
       values={[
         [new Date("November 17, 2020 03:24:00"), 20.2],
         [new Date("November 17, 2020 06:24:00"), 20.2],
-        [new Date('November 19, 2020 12:24:00'), 42.2],
+        [new Date("November 19, 2020 12:24:00"), 42.2],
         [new Date('November 20, 2020 12:24:00'), 53.2]
       ]}
     />
