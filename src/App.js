69a3e1d136d70eacfe0bbf7a420465390582e19b
Bartlomiej Skronski <bartlomiej.skronski@neoteric.eu>
2020-12-07 01:10:13 +0100

    run prettier

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navigation from './components/pages/Navigation';
-import NameForm from "./components/pages/StatedNameForm";
-import GraphForm from "./components/pages/StatedGraphForm";
+import NameForm from './components/pages/StatedNameForm';
+import GraphForm from './components/pages/StatedGraphForm';
 
 const App = () => (
   <div className="App">
