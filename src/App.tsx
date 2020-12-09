f36de594ced343c27cfeb33d17e9b1174cddd850
lukas <wski.lukas@gmail.com>
2020-12-09 15:32:59 +0100

    issue30/ fixed bugs in router, chart and input

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import Chart from "./components/Chart";
-import StatedBalanceForm from "./components/StatedBalanceForm";
-import BalanceService from "./components/BalanceService";
 
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import StoreProvider from './storeContext';
@@ -12,9 +10,7 @@ import GraphForm from "./components/pages/StatedGraphForm";
 const App = () => (
     <StoreProvider>
       <div className="App">
-          <StatedBalanceForm balanceService={BalanceService.instance}/>
-          <Chart
-              values={[[new Date("November 17, 2020 03:24:00"), 20.20], [new Date("November 17, 2020 06:24:00"), 20.20], [new Date("November 19, 2020 12:24:00"), 42.20], [new Date("November 20, 2020 12:24:00"), 53.20]]}/>
+          
         <BrowserRouter>
           <div>
           <Navigation />
--- src/components/pages/StatedNameForm.js
+++ src/components/pages/StatedNameForm.js
@@ -1,14 +1,15 @@
 import React from "react";
-import StatedBalanceForm from "../StatedBalanceForm";
 import {getValues, saveValues} from "../../core/Memory";
+
+import StatedBalanceForm from "../StatedBalanceForm";
+import BalanceService from "../BalanceService";
 
 let NameForm = () => {
   return (
-    <div>
-      
+    <>
       <h1>MainPage</h1>
-      <StatedBalanceForm getValues={getValues} saveValues={saveValues}/>
-    </div>
+      <StatedBalanceForm balanceService={BalanceService.instance}/>
+    </>
   );
 }
 
