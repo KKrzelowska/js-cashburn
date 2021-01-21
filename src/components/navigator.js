43bb11c84b01d03acae2c05caa44c0107b1e8d13
Jarosław Kotowski <jaroslaw.kotowski@live.umcs.edu.pl>
2021-01-21 18:12:39 +0100

    modified:   .eslintrc.json modified:   package-lock.json 	modified:   package.json modified:   src/components/navigator.js

--- src/components/navigator.js
+++ src/components/navigator.js
@@ -1,8 +1,4 @@
-import * as React from 'react'
-import { Button, View, Text } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
 
 export function goto ({ route }) {
-  navigation.navigate({ route })
+ navigation.navigate({ route })
 }
