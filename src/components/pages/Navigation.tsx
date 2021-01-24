f7b920a126a248f3ecfe08c213a40c99d63a1ef3
Jarosław Kotowski <jaroslaw.kotowski@live.umcs.edu.pl>
2021-01-24 21:40:47 +0100

    modified:   .eslintrc.json 	modified:   .husky/pre-commit 	modified:   package-lock.json 	modified:   package.json 	modified:   src/components/pages/Navigation.tsx

--- src/components/pages/Navigation.tsx
+++ src/components/pages/Navigation.tsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Navigation = () => {
   return (
     <div>
-      <NavLink to="/">Home </NavLink>
+      <NavLink to="/">Home</NavLink>
       <NavLink to="/graph">graph</NavLink>
     </div>
   );
